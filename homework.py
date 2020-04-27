260137c9339a0dba0edaf6827d0bafa630d017cc
Сергей <sag163@mail.ru>
2020-04-27 19:50:02 +0400

    add project

--- homework.py
+++ homework.py
@@ -3,18 +3,11 @@ import requests
 import telegram
 import time
 from dotenv import load_dotenv
-
 load_dotenv()
 
-
-PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN")
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
-
-
 def parse_homework_status(homework):
-    homework_name = ...
-    if ...
+    homework_name = homework['homework_name']
+    if homework['status'] != 'approved':
         verdict = 'К сожалению в работе нашлись ошибки.'
     else:
         verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
@@ -22,19 +15,26 @@ def parse_homework_status(homework):
 
 
 def get_homework_statuses(current_timestamp):
-    ...
-    homework_statuses = ...
+    PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN")
+    headers = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+    url =  'https://praktikum.yandex.ru/api/user_api/homework_statuses/' 
+    params = {
+        'from_date':current_timestamp
+    }
+    homework_statuses = requests.get(url, params=params, headers=headers)
     return homework_statuses.json()
 
 
 def send_message(message):
-    ...
-    return bot.send_message(...)
-
+    TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+    CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+    proxy_url = os.getenv('proxy_url')
+    proxy = telegram.utils.request.Request(proxy_url=proxy_url) 
+    bot = telegram.Bot(token=TELEGRAM_TOKEN, request=proxy)
+    return bot.send_message(chat_id=CHAT_ID, text=message)
 
 def main():
     current_timestamp = int(time.time())  # начальное значение timestamp
-
     while True:
         try:
             new_homework = get_homework_statuses(current_timestamp)
